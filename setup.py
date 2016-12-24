460340e466d53e006edd3ea9045c16dc7c1ff195
Scott Doucet <duroktar@gmail.com>
2016-12-24 17:58:03 -0400

    Added classifiers and keywords to setup.py

--- setup.py
+++ setup.py
@@ -6,14 +6,25 @@ setup(
     url='https://github.com/Duroktar/YuleLog',
     packages=['yule_log'],
     package_data = {'yule_log': ['yule_log.ico']},
-    license='MIT',
+    license='MIT License',
     author='Scott Doucet',
     author_email='duroktar@gmail.com',
     description='Terminal based X-Mas Yule Log Fireplace',
     install_requires=["asciimatics"],
+    keywords='christmas yule fireplace,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: End Users/Desktop',
+        'Topic :: Games/Entertainment',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+    ],
     entry_points={
           'console_scripts': [
               'YuleLog = yule_log.__main__:main'
           ]
-      }
+    },
 )
